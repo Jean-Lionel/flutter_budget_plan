5e2e938a274a1672ad8c73607a72b985d0ccf41f
Jean-Lionel <nijeanlionel@gmail.com>
2023-02-26 07:21:13 +0200

    set oriantation mobile

--- lib/widget/chart_bar.dart
+++ lib/widget/chart_bar.dart
@@ -9,47 +9,59 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: Text('\$${spedingAmount.toStringAsFixed(0)}'),
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
+    return LayoutBuilder(
+      builder: (context, chart) {
+        return Column(
+          children: [
+            Container(
+              height: chart.maxHeight * 0.15,
+              child: Text('\$${spedingAmount.toStringAsFixed(0)}'),
+            ),
+            SizedBox(
+              height: chart.maxHeight * 0.05,
+            ),
+            Container(
+              height: chart.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      color: Color.fromARGB(220, 200, 195, 195),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                  color: Color.fromARGB(220, 200, 195, 195),
-                  borderRadius: BorderRadius.circular(10),
-                ),
+                  FractionallySizedBox(
+                    heightFactor: spedingAmountTotal,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  )
+                ],
               ),
-              FractionallySizedBox(
-                heightFactor: spedingAmountTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        Text(label),
-      ],
+            ),
+            SizedBox(
+              height: chart.maxHeight * 0.05,
+            ),
+            Container(
+              height: chart.maxHeight * 0.1,
+              child: Flexible(
+                fit: FlexFit.tight,
+                child: Text(label),
+              ),
+            ),
+          ],
+        );
+      },
     );
+
+    
   }
 }
